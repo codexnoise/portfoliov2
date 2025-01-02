cd7dcbf16bfb3be98e8452f1b3738ddb3e620a84
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2025-01-02 16:29:50 -0500

    chore: Remove unnecessary base path in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  base: '/portfoliov2/',
   site: 'https://codexnoise.github.io/portfoliov2/'
 })
